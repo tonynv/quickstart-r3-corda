705512740fb576a7ee71f2148c7cab5304512c46
Tony Vattathil <avattathil@gmail.com>
2018-04-24 12:01:11 -0700

    Update:  Corda versions

--- scripts/install_corda.sh
+++ scripts/install_corda.sh
@@ -3,6 +3,6 @@ echo "Starting Corda Install"
 sudo adduser --system --no-create-home --group corda
 sudo mkdir /opt/corda; sudo chown corda:corda /opt/corda
 cd /opt/corda
-sudo wget -O corda.jar https://r3.bintray.com/corda/net/corda/corda/1.0.0/corda-1.0.0.jar
-sudo wget -O corda-webserver.jar http://r3.bintray.com/corda/net/corda/corda-webserver/1.0.0/corda-webserver-1.0.0.jar
+sudo wget -O corda.jar https://r3.bintray.com/corda/net/corda/corda/3.1-corda/corda-3.1-corda.jar
+sudo wget -O corda-webserver.jar https://r3.bintray.com/corda/net/corda/corda-webserver/3.1-corda/corda-webserver-3.1-corda.jar
 sudo mkdir /opt/corda/cordapps
