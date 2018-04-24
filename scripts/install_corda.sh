c844fa9e6adea41c30add61cc6a18834caf68fe3
Tony Vattathil <avattathil@gmail.com>
2018-04-24 11:16:36 -0700

    Added Austin's snippets

--- scripts/install_corda.sh
+++ scripts/install_corda.sh
@@ -1,2 +1,8 @@
+# Need to add verfication logic
 echo "Starting Corda Install"
-touch /home/ubuntu/corda-install.log
+sudo adduser --system --no-create-home --group corda
+sudo mkdir /opt/corda; sudo chown corda:corda /opt/corda
+cd /opt/corda
+sudo wget -O corda.jar https://r3.bintray.com/corda/net/corda/corda/1.0.0/corda-1.0.0.jar
+sudo wget -O corda-webserver.jar http://r3.bintray.com/corda/net/corda/corda-webserver/1.0.0/corda-webserver-1.0.0.jar
+sudo mkdir /opt/corda/cordapps
