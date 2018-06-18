c2361e3d95621806a481013e16cf04eee4b7826f
Tommy Lillehagen <tommy.lillehagen@r3.com>
2018-06-18 15:50:40 +0100

    ENT-2102 - Update config and add PostgreSQL driver download

--- scripts/install_corda.sh
+++ scripts/install_corda.sh
@@ -1,9 +1,18 @@
+#!/bin/bash
+
+set -euo pipefail
+
 # Need to add verfication logic
 echo "Starting Corda Install"
 sudo adduser --system --no-create-home --group corda
-sudo mkdir /opt/corda; sudo chown corda:corda /opt/corda
+sudo mkdir /opt/corda
+sudo chown corda:corda /opt/corda
+
 cd /opt/corda
 sudo wget -O corda.jar https://r3.bintray.com/corda/net/corda/corda/3.1-corda/corda-3.1-corda.jar
 sudo wget -O corda-webserver.jar https://r3.bintray.com/corda/net/corda/corda-webserver/3.1-corda/corda-webserver-3.1-corda.jar
 sudo mkdir /opt/corda/cordapps
 sudo mkdir /opt/corda/certificates
+
+sudo mkdir /opt/corda/drivers
+sudo wget -O drivers/postgresql-42.2.2.jar https://jdbc.postgresql.org/download/postgresql-42.2.2.jar
