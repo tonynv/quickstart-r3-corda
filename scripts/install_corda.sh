069416ad9fada852d3d53368aeb695a5c234037c
Tommy Lillehagen <tommy.lillehagen@r3.com>
2018-06-21 08:51:24 +0100

    ENT-2102 - Constraints, logging and tidy-up

--- scripts/install_corda.sh
+++ scripts/install_corda.sh
@@ -4,30 +4,33 @@ set -euo pipefail
 
 # Parameters
 
-IP_ADDRESS=${1:-0.0.0.0}
-COUNTRY_CODE=${2:-UK}
-LOCATION=${3:-London}
-DB_URL="$(echo "${4:-localhost}" | sed 's#/#\\/#g')" # TODO: Fix
-DB_SCHEMA=${5:-CordaDB}
-DB_USER=${6:-CordaUser}
-DB_PWD=${7:-}
-ONE_TIME_DOWNLOAD_KEY=${8:-}
+IP_ADDRESS="${1:-0.0.0.0}"
+COUNTRY_CODE="$(echo "${2:-UK}" | sed 's/[^A-Za-z]//g')"
+LOCATION="$(echo "${3:-London}" | sed 's/[^A-Za-z -]//g')"
+DB_URL="${4:-}"
+DB_SCHEMA="public"
+DB_USER="${5:-CordaUser}"     # Constrained by template
+DB_PWD="${6:-}"               # Constrained by template
+ONE_TIME_DOWNLOAD_KEY="$(echo "${7:-}" | sed 's/[^A-Za-z0-9-]//g')"
 
 # Constants
 
 INSTALL_DIR="/opt/corda"
 NODE_CONFIG_FILE="/opt/corda/node.conf"
-VERSION=3.1-corda
+
+ # TODO: Change to production endpoint before release; https://testnet.corda.network
 TESTNET_URL="https://cces.corda.r3cev.com"
 
-FLAVOUR="AWS"
+PLATFORM="AWS"
+DISTRO="ENT"
+
 DRIVER_FILE="postgresql-42.2.2.jar"
 DRIVER_URL="https://jdbc.postgresql.org/download/$DRIVER_FILE"
 
 # Helper functions
 
 log() {
-    echo "$(date +'%Y-%m-%d %H:%M:%S') $@"
+    echo "$(date +'%Y-%m-%d %H:%M:%S') [INFO] $@"
 }
 
 download() {
@@ -35,31 +38,36 @@ download() {
 }
 
 error() {
-    echo "$(date +'%Y-%m-%d %H:%M:%S') Error: $@" >> /dev/stderr
+    echo "$(date +'%Y-%m-%d %H:%M:%S') [ERROR] $@" >> /dev/stderr
     exit 1
 }
 
-# Install dependencies
+# Fail fast if one-time-key has not been set
 
-log "Installing on IP=${IP_ADDRESS} in location='${LOCATION}, ${COUNTRY_CODE}'"
-log "Using database '${DB_URL}' with schema '${DB_SCHEMA}' and user '${DB_USER}'"
+log "Installing on IP=${IP_ADDRESS} in location='${LOCATION}, ${COUNTRY_CODE}' ..."
+log "Using database '${DB_URL}' with schema '${DB_SCHEMA}' and user '${DB_USER}' ..."
 
 if [ -z "$ONE_TIME_DOWNLOAD_KEY" ]; then
     error "One time download key not provided"
 fi
 
-log "Using one time download key '${ONE_TIME_DOWNLOAD_KEY}''"
+log "Using one time download key '${ONE_TIME_DOWNLOAD_KEY}' ..."
+
+# Install dependencies
+
+log "Updating package manager cache ..."
+
+sudo apt-get update -yqq
 
 log "Installing dependencies ..."
 
-sudo apt-get update -yqq
 sudo apt-get install -yqq \
     apt-transport-https ca-certificates curl software-properties-common \
     openjdk-8-jdk unzip
 
-# Install Corda
+# Set up Corda installation directory
 
-log "Installing Corda ..."
+log "Installing Corda into '$INSTALL_DIR' ..."
 
 sudo adduser --system --no-create-home --group corda
 sudo mkdir -p "$INSTALL_DIR"
@@ -69,43 +77,46 @@ sudo mkdir -p "$INSTALL_DIR/certificates"
 sudo mkdir -p "$INSTALL_DIR/drivers"
 cd "$INSTALL_DIR"
 
-# Download driver JAR for MS SQL Server
+# Download and install database driver JAR
 
-log "Downloading JDBC database drivers ..."
+log "Downloading JDBC database driver ..."
 
 download "$DRIVER_URL" "drivers/$DRIVER_FILE"
 
-# Cache config file locally and patch it
+log "Installed JDBC database driver '$DRIVER_FILE'"
 
-log "Setting up node configuration and retrieving identity and truststore ..."
+# Download configuration, identity, truststore and binaries
 
-sudo curl -L \
-    -d "{\"x500Name\":{\"locality\":\"$LOCATION\",\"country\":\"$COUNTRY_CODE\"},\"configType\":\"$FLAVOUR\"}" \
+log "Retrieving configuration file, identity, truststore and binaries from '$TESTNET_URL' ..."
+
+# TODO: Incorporate $DISTRO when supported by endpoint
+sudo curl -L -s \
+    -d "{\"x500Name\":{\"locality\":\"$LOCATION\",\"country\":\"$COUNTRY_CODE\"},\"configType\":\"$PLATFORM\"}" \
     -H "Content-Type: application/json" \
     -X POST "$TESTNET_URL/api/user/node/generate/one-time-key/redeem/$ONE_TIME_DOWNLOAD_KEY" \
     -o "$INSTALL_DIR/corda.zip" || error "Unable to download config template and truststore"
 
-sudo unzip /opt/corda/corda.zip || error "Unable to unzip generated node bundle; was the correct one time download key used?"
+log "Distribution bundle stored to '$INSTALL_DIR/corda.zip'"
+
+log "Unpacking distribution ..."
+
+sudo unzip /opt/corda/corda.zip 2> /dev/null || error "Unable to unzip generated node bundle; was the correct one time download key used?"
+
+log "Setting permissions to certificates directory ..."
+
+sudo chown -R corda:corda certificates
 
 log "Patching configuration file ..."
 
-sudo sed -i "s/__IPADDRESS__/$IP_ADDRESS/g" "$NODE_CONFIG_FILE" || error "Failed to set IP address in config"
-sudo sed -i "s/__DATASOURCE_URL__/jdbc:postgresql:\/\/$DB_URL/g" "$NODE_CONFIG_FILE" || error "Failed to set database URL in config"
+ESC_DB_URL="$(echo "$DB_URL" | sed 's#/#\\/#g')"
+sudo sed -i "s/__IPADDRESS__/0.0.0.0/g" "$NODE_CONFIG_FILE" || error "Failed to set IP address in config"
+sudo sed -i "s/__DATASOURCE_URL__/jdbc:postgresql:\/\/$ESC_DB_URL/g" "$NODE_CONFIG_FILE" || error "Failed to set database URL in config"
 sudo sed -i "s/__DATASOURCE_USER__/$DB_USER/g" "$NODE_CONFIG_FILE" || error "Failed to set database username in config"
 sudo sed -i "s/__DATASOURCE_PASSWORD__/$DB_PWD/g" "$NODE_CONFIG_FILE" || error "Failed to set database password in config"
 sudo sed -i "s/__DATABASE_SCHEMA_NAME__/$DB_SCHEMA/g" "$NODE_CONFIG_FILE" || error "Failed to set database schema name in config"
-
-# TODO: Set if flavour is Enterprise (from redeem endpoint further up)
-# sudo tee "$NODE_CONFIG_FILE" <<EOF
-#     enterpriseConfiguration = {
-#         mutualExclusionConfiguration = {
-#             on = true
-#             updateInterval = 20000
-#             waitInterval = 40000
-#         }
-#     }
-# EOF > /dev/null
+sudo sed -i "s/^[ ]*\"extraAdvertisedServiceIds\".*$//" "$NODE_CONFIG_FILE" || error "Failed to remove 'extraAdvertisedServiceIds' config"
+sudo sed -i "s/^[ ]*\"webAddress\".*$//" "$NODE_CONFIG_FILE" || error "Failed to remove 'webAddress' config"
 
 log "Node configuration completed"
 
-log "Installation complete"
+log "Installation successfully completed"
